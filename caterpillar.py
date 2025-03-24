1865651d096b4b5ae4c3ec53e2e8f0494f67b50d
G. Mh. <152092729+Pugneum-H@users.noreply.github.com>
2025-03-24 18:31:55 +0300

    Create caterpillar.py
    Add v1 parser, after some testing

---
+++ caterpillar.py
@@ -0,0 +1,103 @@
+import re
+
+def caterpillar(text_ : str, ):     
+
+
+    text = text_
+
+    patterns = [    
+    #   REGEX                                   COMMENT                 IGNORE INNER REGEX?   
+        r'^@([1-6])\s(.*?)$',                   # headings                  
+        r'^>\s(.*?)$',                          # blockquote                
+        r'^#\s(.*?)$',                          # code (full line)      !   
+        r'\*\*(.*?)\*\*',                       # bold                      
+        r'//(.*?)//',                           # italic                    
+        r'__(.*?)__',                           # underline                 
+        r'--(.*?)--',                           # strikethrough             
+        r'##(.*?)##',                           # highlighted               
+        r'``(.*?)``',                           # code (inline)         !   
+        r'\!\((.*?)\)\[(.*?)\]',                # link                  !   
+        r'\!\[(.*?)\]\[(.*?)\]\[(.*?):(.*?)\]', # picture               !   
+        r'^\+\s(.*?)$',                         # list (unordered)          
+        r'\|\!\|(.*?)\|\!\|'                    # leave as-is (block)    !   
+    ]
+    capture_patterns = [                                  
+        r'^@[1-6]\s.*?$',                   
+        r'^>\s.*?$',                          
+        r'^#\s.*?$',                          
+        r'\*\*.*?\*\*',                       
+        r'//.*?//',                           
+        r'__.*?__',                           
+        r'--.*?--',                           
+        r'##.*?##',                           
+        r'``.*?``',                           
+        r'\!\(.*?\)\[.*?\]',                
+        r'\!\[.*?\]\[.*?\]\[.*?:.*?\]', 
+        r'^\+\s.*?$',                         
+        r'\|\!\|.*?\|\!\|'                      
+    ]
+
+    # PARSING 
+     
+    # WORKING WITH THOSE WITH "!" ("I" column) - left as-is no other regex parsing
+    as_is_elements = []
+    as_is_elements.extend(re.findall(patterns[2], text, re.MULTILINE))
+    text = re.sub(capture_patterns[2], "# text", text, 0, re.MULTILINE)
+    as_is_elements.extend(re.findall(patterns[8], text, re.DOTALL))
+    text = re.sub(capture_patterns[8], "``text``", text, 0, re.DOTALL)
+    as_is_elements.extend(re.findall(patterns[9], text))
+    text = re.sub(capture_patterns[9], "!(text)[text]", text, 0)
+    as_is_elements.extend(re.findall(patterns[10], text))
+    text = re.sub(capture_patterns[10], "![text][text][text:text]", text, 0)
+    as_is_elements.extend(re.findall(patterns[12], text, re.DOTALL))
+    text = re.sub(capture_patterns[12], "|!|text|!|", text, 0, re.DOTALL)
+
+
+    # HEADINGS
+    text = re.sub(patterns[0], r'<h\1>\2</h\1>', text, 0, re.MULTILINE)
+
+    # TEXT MODIFIERS
+    text = re.sub(patterns[3], r'<b>\1</b>', text, 0, re.DOTALL)
+    text = re.sub(patterns[4], r'<i>\1</i>', text, 0, re.DOTALL)
+    text = re.sub(patterns[5], r'<u>\1</u>', text, 0, re.DOTALL)
+    text = re.sub(patterns[6], r'<s>\1</s>', text, 0, re.DOTALL)
+    text = re.sub(patterns[7], r'<mark>\1</mark>', text, 0, re.DOTALL)
+
+    # BLOCKQUOTE
+    text = re.sub(patterns[1], r'<blockquote>\1</blockquote>', text, 0, re.MULTILINE)
+
+    # UNORDERED LIST
+    text = re.sub(patterns[11], r'<li>\1</li>', text, 0, re.MULTILINE)
+
+    # REINSERTING AS-IS ELEMENTS
+    idx = 0
+    found_elements = re.findall(capture_patterns[2], text, re.MULTILINE)
+    for i in range(len(found_elements)):
+        text = text.replace(found_elements[i], "<code>"+as_is_elements[idx]+"</code>", 1)
+        idx += 1
+    found_elements = re.findall(capture_patterns[8], text, re.DOTALL)
+    for i in range(len(found_elements)):
+        text = text.replace(found_elements[i], "<code>"+as_is_elements[idx]+"</code>", 1)
+        idx += 1
+    found_elements = re.findall(capture_patterns[9], text, re.DOTALL)
+    for i in range(len(found_elements)):
+        text = text.replace(found_elements[i], "<a href="+as_is_elements[idx][0]+">"+as_is_elements[idx][1]+"</a>", 1)
+        idx += 1
+    found_elements = re.findall(capture_patterns[10], text, re.DOTALL)
+    for i in range(len(found_elements)):
+        text = text.replace(found_elements[i], f"<img src='{as_is_elements[idx][0]}' alt='{as_is_elements[idx][1]}' width='{as_is_elements[idx][2]}' height='{as_is_elements[idx][3]}'>", 1)
+        idx += 1
+    found_elements = re.findall(capture_patterns[12], text, re.DOTALL)
+    for i in range(len(found_elements)):
+        text = text.replace(found_elements[i], as_is_elements[idx], 1)
+        idx += 1
+
+    text = text.replace("\n", "<br>\n")
+    text = text.replace("<br>\n<br>\n", "<br>\n")
+
+    return text
+
+    
+
+
+
