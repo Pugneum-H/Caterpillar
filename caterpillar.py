9f610450cb3064e10cf908c521de2dce411a4b8b
G. Mh. <152092729+Pugneum-H@users.noreply.github.com>
2025-03-25 20:05:12 +0300

    Update caterpillar.py
    Added settings (esp. "silenced" flag) and started adding plugins support

--- caterpillar.py
+++ caterpillar.py
@@ -1,9 +1,36 @@
 import re
+import importlib
+import logging
 
-def caterpillar(text_ : str, ):     
+def caterpillar(text_ : str, *plugins_, **settings_):
+
+    plugins = plugins_
+    settings = settings_
+    text = text_
+
+    logging.getLogger().handlers = []  # Clear any previous handlers
+
+    if settings.get('silenced', False) == True:
+        logging.basicConfig(
+            level=logging.CRITICAL,  
+            format='%(asctime)s - %(message)s',  
+            handlers=[]  
+        )
+    else:
+        logging.basicConfig(
+            level=logging.INFO,  
+            format='%(asctime)s - %(message)s',  
+            handlers=[logging.StreamHandler()] 
+        )
+    
+
 
 
     text = text_
+
+    logging.info("started parsing")
+
+
 
     patterns = [    
     #   REGEX                                   COMMENT                 IGNORE INNER REGEX?   
@@ -19,8 +46,9 @@ def caterpillar(text_ : str, ):
         r'\!\((.*?)\)\[(.*?)\]',                # link                  !   
         r'\!\[(.*?)\]\[(.*?)\]\[(.*?):(.*?)\]', # picture               !   
         r'^\+\s(.*?)$',                         # list (unordered)          
-        r'\|\!\|(.*?)\|\!\|'                    # leave as-is (block)    !   
+        r'\|\!\|(.*?)\|\!\|'                    # leave as-is (block)   !   
     ]
+
     capture_patterns = [                                  
         r'^@[1-6]\s.*?$',                   
         r'^>\s.*?$',                          
@@ -52,9 +80,7 @@ def caterpillar(text_ : str, ):
     as_is_elements.extend(re.findall(patterns[12], text, re.DOTALL))
     text = re.sub(capture_patterns[12], "|!|text|!|", text, 0, re.DOTALL)
 
-
-    # HEADINGS
-    text = re.sub(patterns[0], r'<h\1>\2</h\1>', text, 0, re.MULTILINE)
+    logging.info("removed as-is elements")
 
     # TEXT MODIFIERS
     text = re.sub(patterns[3], r'<b>\1</b>', text, 0, re.DOTALL)
@@ -62,12 +88,20 @@ def caterpillar(text_ : str, ):
     text = re.sub(patterns[5], r'<u>\1</u>', text, 0, re.DOTALL)
     text = re.sub(patterns[6], r'<s>\1</s>', text, 0, re.DOTALL)
     text = re.sub(patterns[7], r'<mark>\1</mark>', text, 0, re.DOTALL)
+    
+    logging.info("parsed text modifiers")
+
+    # HEADINGS
+    text = re.sub(patterns[0], r'<h\1>\2</h\1>', text, 0, re.MULTILINE)
+    logging.info("parsed headings")
 
     # BLOCKQUOTE
     text = re.sub(patterns[1], r'<blockquote>\1</blockquote>', text, 0, re.MULTILINE)
+    logging.info("parsed blockquotes")
 
     # UNORDERED LIST
     text = re.sub(patterns[11], r'<li>\1</li>', text, 0, re.MULTILINE)
+    logging.info("parsed lists")
 
     # REINSERTING AS-IS ELEMENTS
     idx = 0
@@ -91,9 +125,21 @@ def caterpillar(text_ : str, ):
     for i in range(len(found_elements)):
         text = text.replace(found_elements[i], as_is_elements[idx], 1)
         idx += 1
+    logging.info("inserted as-is elements")
 
     text = text.replace("\n", "<br>\n")
     text = text.replace("<br>\n<br>\n", "<br>\n")
+    logging.info("parsing newlines")
+
+    for plugin in plugins:
+        try:
+            plug = importlib.import_module(plugin)
+            text, plugins, settings = plug.main(text, patterns, capture_patterns, plugins, settings)
+            logging.info("plugin '" + str(plugin) + "' loaded and executed")
+        except:
+            logging.warning("plugin '"+ str(plugin) +"' failed to load and execute")
+        
+        
 
     return text
 
